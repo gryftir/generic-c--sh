b300aa77208c5ec3a041c163a5f098374732472f
Lawrence Siebert <gryftir@gmail.com>
2014-09-13 21:37:10 -0700

    first commit

---
+++ GENERIC.c
@@ -0,0 +1,9 @@
+/*GENERIC c file*/
+#include "GENERIC.h"
+
+
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
---
+++ generic.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+test_exists() {
+    if [ -e $1 ]; then
+        echo "$1 $2 already exists"
+        exit 1
+    fi
+    return 0
+}
+test_exists $1 "folder"
+mkdir $1
+cd $1
+test_exists $1.c
+test_exists $1.h
+cp /home/lawrence/workspace/c/generic/Makefile Makefile
+cp /home/lawrence/workspace/c/generic/GENERIC.c $1.c
+cp /home/lawrence/workspace/c/generic/GENERIC.h $1.h
+cp /home/lawrence/workspace/c/generic/.ycm_extra_conf.py .ycm_extra_conf.py
+cp /home/lawrence/workspace/c/generic/.gitignore .gitignore
+sed -i "s/GENERIC/$1/g" Makefile
+sed -i "s/GENERIC/$1/g" $1.c
+sed -i "s/GENERIC/$1/g" $1.h
+echo "created folder $1"
+
+
+
+
+
+
+
+
+
+
+
+
+
