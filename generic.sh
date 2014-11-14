aea74e810c7ddd029cabf4f27cdf518a909b8183
Lawrence Siebert <gryftir@gmail.com>
2014-11-13 20:24:40 -0800

    print current directory, and script can now be installed at any location

--- generic.sh
+++ generic.sh
@@ -11,17 +11,19 @@ mkdir $1
 cd $1
 test_exists $1.c
 test_exists $1.h
-cp /home/lawrence/workspace/c/generic/Makefile Makefile
-cp /home/lawrence/workspace/c/generic/GENERIC.c $1.c
-cp /home/lawrence/workspace/c/generic/GENERIC-lib.c $1-lib.c
-cp /home/lawrence/workspace/c/generic/GENERIC-lib.h $1-lib.h
-cp /home/lawrence/workspace/c/generic/.ycm_extra_conf.py .ycm_extra_conf.py
-cp /home/lawrence/workspace/c/generic/.gitignore .gitignore
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+cp ${SCRIPT_DIR}/Makefile Makefile
+cp ${SCRIPT_DIR}/GENERIC.c $1.c
+cp ${SCRIPT_DIR}/GENERIC-lib.c $1-lib.c
+cp ${SCRIPT_DIR}/GENERIC-lib.h $1-lib.h
+cp ${SCRIPT_DIR}/.ycm_extra_conf.py .ycm_extra_conf.py
+cp ${SCRIPT_DIR}/.gitignore .gitignore
 sed -i "s/GENERIC/$1/g" Makefile
 sed -i "s/GENERIC-lib/$1-lib/g" $1-lib.c
 sed -i "s/GENERIC-lib/$1-lib/g" $1-lib.h
 sed -i "s/GENERIC/$1/g" $1.c
 echo "created folder $1"
+echo "current directory: `pwd`"
 
 
 
